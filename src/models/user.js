c736986eaf2c383292147175e2faad0ed0b9ab34
Login Viktor <batazor111@gmail.com>
2015-04-28 19:23:21 -0700

    Update model Mongoose - User

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
-var userSchema = mongoose.Schema({
+var UserSchema = mongoose.Schema({
 
   local: {
     email:     String,
@@ -12,14 +12,14 @@ var userSchema = mongoose.Schema({
 
 // methods =====================================================================
 // generating a hash
-userSchema.methods.generateHash = function(password) {
+UserSchema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-userSchema.methods.validPassword = function(password) {
+UserSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.local.password);
 };
 
 // create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
