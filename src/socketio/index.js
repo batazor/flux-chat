472adfaedf9eb0f060b60ce35a95bbcd88b39809
Victor Login <batazor111@gmail.com>
2017-04-30 04:36:40 +0300

    Fix socket

--- src/socketio/index.js
+++ src/socketio/index.js
@@ -1,21 +1,21 @@
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const passportSocketIo = require('passport.socketio');
-const User = require('../models/user');
-const config = require('../config')(process.env.NODE_ENV);
+var cookieParser = require("cookie-parser");
+var session      = require('express-session');
+var MongoStore   = require('connect-mongo')(session);
+var passportSocketIo = require("passport.socketio");
+var User = require('../models/user');
+var config = require('../config')(process.env.NODE_ENV);
 
-module.exports = function (io) {
-  io.on('connection', socket => {
+module.exports = function(io) {
+  io.on('connection', function(socket) {
 
     // Save socket.id for authenticated user
     if (socket.client.request.user._id !== undefined) {
       User.update(
-        { _id: socket.client.request.user._id },
-        { $set: { socketID: socket.id } },
-        (err, numberAffected, raw) => {
-          if (err) { return handleError(err); }
-        },
+        {_id: socket.client.request.user._id},
+        {$set: {socketID: socket.id}},
+        function(err, numberAffected, raw){
+          if (err) return handleError(err);
+        }
       );
     }
 
@@ -29,26 +29,26 @@ module.exports = function (io) {
   });
 
   io.use(passportSocketIo.authorize({
-    cookieParser,
-    key: config.session.key,
-    secret: config.session.secret,
-    store: new MongoStore(config.mongo),
-    success: onAuthorizeSuccess,
-    fail: onAuthorizeFail,
+    cookieParser: cookieParser,
+    key:          config.session.key,
+    secret:       config.session.secret,
+    store:        new MongoStore(config.mongo),
+    success:      onAuthorizeSuccess,
+    fail:         onAuthorizeFail,
   }));
 
-  function onAuthorizeSuccess(data, accept) {
+  function onAuthorizeSuccess(data, accept){
     console.log('successful connection to socket.io');
 
     accept();
   }
 
-  function onAuthorizeFail(data, message, error, accept) {
+  function onAuthorizeFail(data, message, error, accept){
 
     accept(null, false);
   }
 
-  io.on('disconnect', () => {
+  io.on('disconnect', function() {
     io.reconnect();
   });
 };
